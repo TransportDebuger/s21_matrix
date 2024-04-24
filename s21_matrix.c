ae5277deabf0518b43b7398dc072bf1c2189ac2e
Artem Ulyanov <74565490+TransportDebuger@users.noreply.github.com>
2024-04-24 06:46:23 +0300

    functions added

--- s21_matrix.c
+++ s21_matrix.c
@@ -11,7 +11,7 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
             result->matrix[i] = (double *)calloc(columns, sizeof(double));
         }
     } else {
-        err_code = RES_INCORECT_MATRIX;
+        err_code = RES_INCORRECT_MATRIX;
     }
 
     return err_code;
@@ -37,7 +37,7 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
       A->rows == B->rows && A->columns == B->columns) {
     for (int row = 0; row < A->rows; row++) {
         for (int col = 0; col < A->columns; col++) {
-            if (fabs(A->matrix[row][col] - B->matrix[row][col]) >= 1E-7) {
+            if (fabs(A->matrix[row][col] - B->matrix[row][col]) >= EQ_PRECISE) {
                 result_code = FAILURE;
                 break;
             }
@@ -67,7 +67,7 @@ int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
             err_code = RES_CALC_ERROR;
         }
     } else {
-        err_code = RES_INCORECT_MATRIX;
+        err_code = RES_INCORRECT_MATRIX;
     }
 
     return err_code;
@@ -89,8 +89,123 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
             err_code = RES_CALC_ERROR;
         }
     } else {
-        err_code = RES_INCORECT_MATRIX;
+        err_code = RES_INCORRECT_MATRIX;
     }
 
     return err_code;
+}
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
+  int err_code = RES_OK;
+
+  if (A != NULL && A->matrix != NULL && A->rows > 0 && A->columns > 0) {
+    s21_create_matrix(A->rows, A->columns, result);
+    for (int row = 0; row < A->rows; row++) {
+      for (int col = 0; col < A->columns; col++) {
+        result->matrix[row][col] = A->matrix[row][col] * number;
+      }
+    }
+  } else {
+    err_code = RES_INCORRECT_MATRIX;
+  }
+
+  return err_code;
+}
+
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+    int err_code = RES_OK;
+
+    if (A != NULL && B != NULL && A->matrix != NULL && B->matrix != NULL &&
+        A->rows > 0 && B->rows > 0 && A->columns > 0 && B->columns > 0) {
+        if (A->columns == B->rows) {
+            s21_create_matrix(A->rows, B->columns, result);
+            for (int row = 0; row < A->rows; row++) {
+                for (int col = 0; col < B->columns; col++) {
+                    for (int i = 0; i < A->columns; i++) {
+                        result->matrix[row][col] += A->matrix[row][i] * B->matrix[i][col];
+                    }
+                }
+            }
+        } else {
+            err_code = RES_CALC_ERROR;
+        }
+    } else {
+        err_code = RES_INCORRECT_MATRIX;
+    }
+
+    return err_code;
+}
+
+int s21_transpose(matrix_t *A, matrix_t *result) {
+  int err_code = RES_OK;
+
+  if (A != NULL && A->matrix != NULL && A->rows > 0 && A->columns > 0) {
+    s21_create_matrix(A->columns, A->rows, result);
+    for (int row = 0; row < A->rows; row++) {
+      for (int col = 0; col < A->columns; col++) {
+        result->matrix[col][row] = A->matrix[row][col];
+      }
+    }
+  } else {
+    err_code = RES_INCORRECT_MATRIX;
+  }
+  
+  return err_code;
+}
+
+int s21_determinant(matrix_t *A, double *result) {
+  int err_code = RES_OK;
+  *result = 0;
+
+  if (A != NULL && A->matrix != NULL && A->rows > 0 && A->columns > 0) {
+    if (A->rows == A-> columns) {
+      if (A->rows == 1) {
+        *result = A->matrix[0][0];
+      } else if (A->rows == 2) {
+        *result = A->matrix[0][0] * A->matrix[1][1] - A->matrix[1][0] * A->matrix[0][1];
+      } else {
+        for (int i = 0; i < A->columns; i++) {
+
+        }
+      }
+    } else {
+      err_code = RES_CALC_ERROR;
+    }
+  } else {
+    err_code = RES_INCORRECT_MATRIX;
+  }
+
+  return err_code;
+}
+
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+  int err_code = 0;
+
+  if (A != NULL && A->matrix != NULL && A->rows > 0 && A->columns > 0) {
+    if (A->rows == A->columns) {
+
+    } else {
+      err_code = RES_CALC_ERROR;
+    }
+  } else {
+    err_code = RES_INCORRECT_MATRIX;
+  }
+
+  return err_code;
+}
+
+int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
+  int err_code = 0;
+
+  if (A != NULL && A->matrix != NULL && A->rows > 0 && A->columns > 0) {
+    if (A->rows == A->columns) {
+
+    } else {
+      err_code = RES_CALC_ERROR;
+    }
+  } else {
+    err_code = RES_INCORRECT_MATRIX;
+  }
+
+  return err_code;
 }
--- s21_matrix.h
+++ s21_matrix.h
@@ -5,12 +5,15 @@
 
 //result codes
 #define RES_OK 0
-#define RES_INCORECT_MATRIX 1
+#define RES_INCORRECT_MATRIX 1
 #define RES_CALC_ERROR 2
 
 //matrix comparison result codes
 #define SUCCESS 1
 #define FAILURE 0
+
+//matrix compare precision
+#define EQ_PRECISE 1e-7
 
 //matrix type definition
 typedef struct matrix_struct {
@@ -19,28 +22,16 @@ typedef struct matrix_struct {
     int columns;
 } matrix_t;
 
-//constructor
 int s21_create_matrix(int rows, int columns, matrix_t *result);
-
-//destructor
 void s21_remove_matrix(matrix_t *A);
-
-//matrix comparison
 int s21_eq_matrix(matrix_t *A, matrix_t *B);
-
-//matrix arithmetic functions
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_mult_number(matrix_t *A, double number, matrix_t *result);
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
-
 int s21_transpose(matrix_t *A, matrix_t *result);
-
 int s21_calc_complements(matrix_t *A, matrix_t *result);
-
 int s21_determinant(matrix_t *A, double *result);
-
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
-
 
 #endif
